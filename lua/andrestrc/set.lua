df0a9af513143d9dbc9fb01de01163d420e2a2ac
Brayan Carreño <andrebrayan74@gmail.com>
2023-12-18 20:05:53 -0500

    chore: some fixes

--- lua/andrestrc/set.lua
+++ lua/andrestrc/set.lua
@@ -3,9 +3,9 @@ vim.opt.guicursor = ""
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
@@ -27,3 +27,5 @@ vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
+vim.opt.clipboard=unnamedplus
+
